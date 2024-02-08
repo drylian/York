309a438916996c8de798548c338dfd53e7821f57
Drylian <109999325+drylian@users.noreply.github.com>
2024-02-08 18:04:14 -0300

    tst

--- Tibia/server/entrypoint.sh
+++ Tibia/server/entrypoint.sh
@@ -9,4 +9,4 @@ MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+${MODIFIED_STARTUP}
