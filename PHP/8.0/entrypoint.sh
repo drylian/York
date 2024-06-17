d2609b33db7e00e120d0935f71093649f22b38b1
Drylian <109999325+drylian@users.noreply.github.com>
2024-06-17 16:59:01 -0300

    att

--- PHP/8.0/entrypoint.sh
+++ PHP/8.0/entrypoint.sh
@@ -4,10 +4,15 @@ sleep 1
 
 cd /home/container
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 __PHP_VERSION="8"
-php -v
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
