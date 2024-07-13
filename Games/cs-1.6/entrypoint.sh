40f4b046dfe337eaba8523ba32fc4e233e34d96b
Drylian <109999325+drylian@users.noreply.github.com>
2024-07-13 18:46:38 -0300

    arr

--- Games/cs-1.6/entrypoint.sh
+++ Games/cs-1.6/entrypoint.sh
@@ -13,17 +13,6 @@ export INTERNAL_IP
 
 cd /home/container || exit 1
 
-# Check if the 'installed' file exists 
-if [ ! -f "./installed" ]; then
-  wget wget https://downloads.csrevo.com/hlds/base/rehlds_full_linux.zip
-  unzip rehlds_full_linux.zip .
-  cp -v linux64/steamclient.so ../.steam/sdk64/steamclient.so
-    # Create the 'installed' file to mark that the installation is complete
-    touch ../installed
-  fi
-  
-chmod -R 755 /home/container
-
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
