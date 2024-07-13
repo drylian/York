6bed2dde963ab5b93e284025ca3a3d5cf9785575
Drylian <109999325+drylian@users.noreply.github.com>
2024-07-13 15:05:07 -0300

    ATT

--- Games/cs-1.6/entrypoint.sh
+++ Games/cs-1.6/entrypoint.sh
@@ -16,7 +16,14 @@ cd /home/container || exit 1
 # Check if the 'installed' file exists 
 if [ ! -f "./installed" ]; then
   cd /home/container/steamcmd || exit 1
-
+ echo -e "----------------------------------------------------------------------------------"
+        echo -e " WARNING!!! Wait, this can take around 10 minutes "
+        echo -e " if it is canceled this can and will cause problems in the code,"
+        echo -e " so wait, this procedure only occurs once"
+        echo -e " AVISO!!! Aguarde, isso pode demorar em torno de 10 minutos"
+        echo -e " caso seja cancelado isso pode e vai causar problemas no codigo,"
+        echo -e "  então espere, esse procedimento ocorre apenas uma vez"
+        echo -e "----------------------------------------------------------------------------------"
   while test "$status" != "Success! App '90' fully installed."; do 
     status=$(./steamcmd.sh +login anonymous \
     +force_install_dir /home/container +app_update 90 validate +quit | \
@@ -24,7 +31,7 @@ if [ ! -f "./installed" ]; then
   done
 
   git clone https://github.com/hpoon/HLDS-CS1.6
-  mv -rf ./HLDS-CS1.6/cstrike /home/container/cstrike
+  mv ./HLDS-CS1.6/cstrike/* /home/container/cstrike
   rm -r ./HLDS-CS1.6
 
   # Create the 'installed' file to mark that the installation is complete
