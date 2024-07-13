8ed50e4e2d01580291ece60a1165bae25985b461
Drylian <109999325+drylian@users.noreply.github.com>
2024-07-13 14:39:11 -0300

    add cs-1.6 docker test

---
+++ Games/cs-1.6/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Wait for the container to fully initialize
+sleep 1
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+cd /home/container || exit 1
+
+# Check if the 'installed' file exists
+if [ ! -f "./installed" ]; then
+  while test "$status" != "Success! App '90' fully installed."; do 
+    status=$(./steamcmd.sh +login anonymous \
+    +force_install_dir /home/container +app_update 90 validate +quit | \
+    tail -1)
+  done
+
+  # Create the 'installed' file to mark that the installation is complete
+  touch ./installed
+fi
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
